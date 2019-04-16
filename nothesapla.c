5a48b53d918e8e0349e224b597ad7c487a866f81
cagriyargi <49692049+cagriyargi@users.noreply.github.com>
2019-04-16 20:27:19 +0300

    Create nothesapla.c

---
+++ nothesapla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int a;
+    
+printf("LuTFEN oGRENCiNiN NOTUNU GiRiNiZ");
+    
+scanf("%d",&a);
+    
+if(a>=90)
+    
+printf("AA\n");
+    
+else if(a>80 && a<=89)
+    
+printf("BA\n");
+    
+else if(a>=70 && a<=80)
+    
+printf("BB\n");
+    
+else 
+    
+printf("F\n");
+
+}
